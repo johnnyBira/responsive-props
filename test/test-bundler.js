dc1c506c75bfba0e27f005f263eeab68a57f21b4
John Persson <john.persson@humblebee.se>
2019-03-26 00:05:13 +0100

    chore(jest): config jest with enzyme

--- test/test-bundler.js
+++ test/test-bundler.js
@@ -1,13 +1,15 @@
 import "jest-enzyme";
 import "jest-styled-components";
 import { JSDOM } from "jsdom";
-import { configure } from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 // import shallowWithTheme from './utils/shallowWithTheme';
 
 const jsdom = new JSDOM("<!doctype html><html><body></body></html>");
-global.document = jsdom.doc;
-global.window = jsdom.defaultView;
+const { window } = jsdom;
+
+global.window = window;
+global.document = window.document;
 // global.shallowWithTheme = shallowWithTheme;
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
